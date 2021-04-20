8e05c4149bf17c0f99e943ea380108009973d748
Aviv <avivcd123@gmail.com>
2021-04-21 00:39:43 +0300

    wip

--- WebCrawler/ConnectToDB.cs
+++ WebCrawler/ConnectToDB.cs
@@ -2,18 +2,42 @@
 using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace WebCrawler
 {
     public static class ConnectToDB
     {
         //Format sql command for inserting product to DB
-        private static string GetFormattedProductInsertQuery(string id, string barcode, string name, string ingredients,
-            string allergy)
+        
+        private static string GetFormattedProductInsertQuery(RecipeData recipeData)
         {
-            return $"insert into Products (Id, Barcode, Name, Ingredients, Allergy values ({id}, {barcode}, {name}, {ingredients}, {allergy})";
+            return $"insert into DatabaseName.TableName (recipeId, recipeName, recipeCategory, recipeCalories, recipeIngredients, recipeInstructions, recipeImageUrl) values" +
+                $" ({recipeData.Id}, {recipeData.Name}, {recipeData.Category}, {recipeData.Calories}, {recipeData.Ingredients}," +
+                $" {recipeData.instructions}, {recipeData.ImageUrl})";
         }
-
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {       
+                string MyConnection2 = "datasource=localhost;port=3307;username=root;password=root";
+                string Query = "insert into student.studentinfo(idStudentInfo,Name,Father_Name,Age,Semester) values('" + this.IdTextBox.Text + "','" + this.NameTextBox.Text + "','" + this.FnameTextBox.Text + "','" + this.AgeTextBox.Text + "','" + this.SemesterTextBox.Text + "');";
+                MySqlConnection MyConn2 = new MySqlConnection(MyConnection2);
+                MySqlCommand MyCommand2 = new MySqlCommand(Query, MyConn2);
+                MySqlDataReader MyReader2;
+                MyConn2.Open();
+                MyReader2 = MyCommand2.ExecuteReader();      
+                MessageBox.Show("Save Data");
+                while (MyReader2.Read())
+                {
+                }
+                MyConn2.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
         //Insert product to DB asynchronysly
         public static async Task<bool> InsertProductToDbAsync(string id, string barcode, string name, string[] ingredients, string[] allergy)
         {
---
+++ WebCrawler/RecipeData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebCrawler
+{
+    public class RecipeData
+    {
+        public int Id { get; private set; }
+
+        public string Name { get; private set; }
+
+        public int Calories { get; private set; }
+
+        public string[] Ingredients { get; private set; }
+
+        public string[] instructions { get; private set; }
+
+        public string Category { get; private set; }
+
+        public string Author { get; private set; }
+
+        public DateTime DatePublished { get; private set; }
+
+        public string ImageUrl { get; private set; }
+    }
+}
